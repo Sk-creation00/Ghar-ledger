b63e97b9e1267bd699eb68f9fa0712d3b8548d23
Sk-creation00 <saifkhani7789@gmail.com>
2026-09-05 10:01:44 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then((cached) => {
+    caches.match(e.request, {ignoreSearch: true}).then((cached) => {
       const fetchPromise = fetch(e.request).then((res) => {
         const clone = res.clone();
         caches.open(CACHE).then((c) => c.put(e.request, clone));
