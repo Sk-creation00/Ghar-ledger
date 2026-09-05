b63501a880715b885be594c87a18b56a1f631042
Sk-creation00 <saifkhani7789@gmail.com>
2026-09-05 09:04:04 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'ghar-ledger-v2';
+const ASSETS = ['/', '/index.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const fetchPromise = fetch(e.request).then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, clone));
+        return res;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
